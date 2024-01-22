1b5f1eff42683a95c4eb1f345908d45acfd6da50
jonysingla <jony.singla19@gmail.com>
2024-01-22 17:58:43 +0530

    Opaque Type

--- TestApp/TestApp/Protocol/OpaqueTypeVC.swift
+++ TestApp/TestApp/Protocol/OpaqueTypeVC.swift
@@ -12,13 +12,33 @@ class OpaqueTypeVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //Example 1: Opaque Type
         func getAnimal() -> some Animal {
             return Dog()
         }
-    }
-}
+        
+        //Example 2: Opaque type with Generic
+        // Generic function with an opaque return type
+//        let shape1 = drawRandomShape(flag: true)
+//
+//        // Example usage
+////        let shape1 = drawRandomShape(flag: true)
+////        let shape2 = drawRandomShape(flag: false)
 
+//        print(shape1.draw())  // Output: Drawing a Circle
+//        print(shape2.draw())  // Output: Drawing a Square
+    }
+    
+//    func drawRandomShape<T: ShapeOpaque>(flag: Bool) -> some ShapeOpaque {
+//        if flag {
+//            return CircleOpaque()
+//        } else {
+//            SquareOpaque()
+//        }
+////            return flag ? CircleOpaque() : SquareOpaque()
+//    }
+}
+//MARK: 1 Opaque Type
 protocol Animal {
     func makeSound() -> String
 }
@@ -35,4 +55,20 @@ struct Cat: Animal {
     }
 }
 
+////MARK: 2 Opaque type with Generic
+//protocol ShapeOpaque {
+//    func draw() -> String
+//}
+//
+//struct CircleOpaque: ShapeOpaque {
+//    func draw() -> String {
+//        return "Drawing a Circle"
+//    }
+//}
+//
+//struct SquareOpaque: ShapeOpaque {
+//    func draw() -> String {
+//        return "Drawing a Square"
+//    }
+//}
 
