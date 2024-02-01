2388eb77f277ddace57ab27a34f15b23c2155e94
jonysingla <jony.singla19@gmail.com>
2024-02-01 15:29:11 +0530

    [Design Pattern Adapter, Bridge] Implemented with example

--- TestApp/TestApp/DesignPattern/StructuralDesignPattern/AdapterVC.swift
+++ TestApp/TestApp/DesignPattern/StructuralDesignPattern/AdapterVC.swift
@@ -13,5 +13,51 @@ class AdapterVC: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        //Calling Example 1
+        let adaptee = Adaptee()
+        let tar = Adapter(adaptee: adaptee)
+        tar.request()
+        
+        //Calling Example 2
+        let tarTwo = AdapterTwo()
+        tarTwo.request()
+    }
+}
+
+protocol Target {
+    func request()
+}
+
+//MARK: Example 1 -- Class One as Adapter with Protocol -- this will work without protocol also
+class Adapter: Target {
+    
+    //Creating object of class two Adaptee
+    var adaptee: Adaptee
+    
+    init(adaptee: Adaptee) {
+        self.adaptee = adaptee
+    }
+    func request() {
+        //Function calling class two as Adaptee
+        adaptee.specificRequest()
+    }
+}
+
+//MARK: Class Two as Adaptee
+class Adaptee {
+    func specificRequest() {
+        print("Specific request")
+    }
+}
+
+//MARK: Example 2 --
+class AdapteeTwo {
+    func specificRequest() {
+        print("Specific request Two")
+    }
+}
+class AdapterTwo: AdapteeTwo, Target {
+    func request() {
+        specificRequest()
     }
 }
--- TestApp/TestApp/DesignPattern/StructuralDesignPattern/BridgeVC.swift
+++ TestApp/TestApp/DesignPattern/StructuralDesignPattern/BridgeVC.swift
@@ -8,22 +8,120 @@
 import UIKit
 
 class BridgeVC: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        //Example 1 Without Bridge
+        let sedanWithoutBridge = SedanWithoutBridge()
+        let redSedan = RedSedan()
+        redSedan.drive()
+        
+        //Example 1.1 With Bridge
+        let sedan = SedanWithBridge()
+        let redSedanCar = RedCar(car: sedan)
+        redSedanCar.drive()
+        
+        //Example 2
+        let tvRemoteControl = RemoteControl(appliance: TV())
+        tvRemoteControl.turnOn()
+        
+        let fancyVacuumCleanerRemoteControl = RemoteControl(appliance: VacuumCleaner())
+        fancyVacuumCleanerRemoteControl.turnOn()
+    }
+}
+
+//MARK: Example 1 ----
+//MARK: Problem without Bridge
+protocol CarWithoutBridge {
+    func drive()
+}
+
+class SedanWithoutBridge: CarWithoutBridge {
+    func drive() {
+        print("drive a sedan")
+    }
+}
+
+class SUVWithoutBridge: CarWithoutBridge {
+    func drive() {
+        print("drive a SUV")
+    }
+}
+
+/* When adding color in car then we create a seperate class , this is not a solution
+ if you can add multiple color for multple cars, Bridge Provide solution for this. */
+class RedSedan: SedanWithoutBridge {
+    override func drive() {
+        print("drive a red sedan without bridge")
+    }
+}
+
+//MARK: Solution with Bridge
+protocol CarWithBridge {
+    func drive()
+}
+
+protocol ColoredCar {
+    var car: CarWithBridge { get set }
+    //    func drive()
+}
+
+class SedanWithBridge: CarWithBridge {
+    func drive() {
+        print("drive a red sedan")
+    }
+}
+
+class SUVWithBridge: CarWithBridge {
+    func drive() {
+        print("drive a SUV")
+    }
+}
+
+class RedCar: ColoredCar {
+    var car: CarWithBridge
+    
+    init(car: CarWithBridge) {
+        self.car = car
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func drive() {
+        car.drive()
     }
-    */
+}
 
+//MARK: Example 2 ----
+protocol Switch {
+    var appliance: Appliance { get set }
+    func turnOn()
+}
+
+protocol Appliance {
+    func run()
+}
+
+class RemoteControl: Switch {
+    var appliance: Appliance
+    
+    init(appliance: Appliance) {
+        self.appliance = appliance
+    }
+    
+    func turnOn() {
+        self.appliance.run()
+    }
+}
+
+class TV: Appliance {
+    func run() {
+        print("tv turned on");
+    }
+}
+
+class VacuumCleaner: Appliance {
+    func run() {
+        print("vacuum cleaner turned on")
+    }
 }
