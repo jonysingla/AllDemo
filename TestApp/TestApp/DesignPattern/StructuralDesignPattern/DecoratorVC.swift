ed60d02deefd095b6a20a4bee47a956f4ad52459
jonysingla <jony.singla19@gmail.com>
2024-02-01 16:44:47 +0530

    [Design Pattern Decorator] Implemented with example

--- TestApp/TestApp/DesignPattern/StructuralDesignPattern/DecoratorVC.swift
+++ TestApp/TestApp/DesignPattern/StructuralDesignPattern/DecoratorVC.swift
@@ -13,17 +13,59 @@ class DecoratorVC: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let sedan = SedanDecorator()
+        let autonomousSedan = AutonomousCar(car: sedan)
+        autonomousSedan.drive()
+    }
+}
+
+//MARK: Without Decorator Design Pattern
+enum CarTypeDecorator: String {
+    case Sedan = "sedan"
+    case SUV = "SUV"
+}
+
+protocol CarDecorator {
+    var type: CarTypeDecorator{ get }
+    func drive()
+}
+
+class SedanDecorator: CarDecorator {
+    var type: CarTypeDecorator = .Sedan
+    func drive() {
+        print("drive a " + type.rawValue)
+    }
+}
+/*
+ Now it's working fine. If we want to add some more Autonomous classes then we can
+ use same inheritance again, and this will create code complex high, so we can use Decorator Design Pattern
+ and pass value at run time.
+ */
+class AutonomousSedan: SedanDecorator {
+    override func drive() {
+        print("automatically drive a " + type.rawValue)
+    }
+}
+
+//MARK: With Decorator Design Pattern
+class Decorator: CarDecorator {
+    
+    var car: CarDecorator
+    var type: CarTypeDecorator {
+        return car.type
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    init(car: CarDecorator) {
+        self.car = car
     }
-    */
+    
+    func drive() {
+        car.drive()
+    }
+}
 
+class AutonomousCar: Decorator {
+    override func drive() {
+        print("automatically drive a " + type.rawValue)
+    }
 }
