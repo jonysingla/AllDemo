0bcec58e8dabb2976073e765645e5d0b103bb24e
jonysingla <jony.singla19@gmail.com>
2024-02-01 18:01:54 +0530

    [Design Pattern FlyWeight] Implemented with Example

--- TestApp/TestApp/DesignPattern/StructuralDesignPattern/FlyweightVC.swift
+++ TestApp/TestApp/DesignPattern/StructuralDesignPattern/FlyweightVC.swift
@@ -13,17 +13,46 @@ class FlyweightVC: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let factory = FactoryFlyweight()
+        let redSedan = factory.getCar(color: .red)
+        redSedan.drive()
+        
+        let greenSedan = factory.getCar(color: .green)
+        greenSedan.drive()
     }
+}
+
+protocol CarFlyWeight {
+    var color: UIColor { get }
+    func drive()
+}
+
+class SedanFlyWeight: CarFlyWeight {
+    var color: UIColor
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    init(color: UIColor) {
+        self.color = color
     }
-    */
+    func drive() {
+        print("drive a sedan \(color)")
+    }
+}
+//MARK: 
+Flywieght Design Pattern
+/*
+ To checking if data is storage or not, if not then we can store data and use in future.
+ */
 
+class FactoryFlyweight {
+    var cars: [UIColor: CarFlyWeight] = [UIColor: CarFlyWeight]()
+    
+    func getCar(color: UIColor) -> CarFlyWeight {
+        if let car = cars[color] {
+            return car
+        } else {
+            let car = SedanFlyWeight(color: color)
+            cars[color] = car
+            return car
+        }
+    }
 }
