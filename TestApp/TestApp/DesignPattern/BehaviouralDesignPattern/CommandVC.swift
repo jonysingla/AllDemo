2f435c8ba90f2be6857749589f8a8894823999b5
jonysingla <jony.singla19@gmail.com>
2024-02-02 13:57:27 +0530

    [Design Pattern Command] Implementation with example

--- TestApp/TestApp/DesignPattern/BehaviouralDesignPattern/CommandVC.swift
+++ TestApp/TestApp/DesignPattern/BehaviouralDesignPattern/CommandVC.swift
@@ -3,7 +3,7 @@
 //  TestApp
 //
 //  Created by Jony on 02/02/24.
-//
+// Ref: https://dev.to/sergeyleschev/swift-design-patterns-command-pattern-3044
 
 import UIKit
 
@@ -11,19 +11,59 @@ class CommandVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let podBayDoors = "Doors"
+        let doorModule = DoorsOperations(doors:podBayDoors)
+        
+        //Command will execute here with help of objects
+        doorModule.open()
+        doorModule.close()
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+//MARK: Another Example
+protocol DoorCommand {
+    func execute() -> String
+}
+
+class OpenCommand: DoorCommand {
+    let doors:String
+    required init(doors: String) {
+        self.doors = doors
     }
-    */
+    func execute() -> String {
+        print("Opened \(doors)")
+        return "Opened \(doors)"
+    }
+}
 
+class CloseCommand: DoorCommand {
+    let doors:String
+
+    required init(doors: String) {
+        self.doors = doors
+    }
+    func execute() -> String {
+        print("Closed \(doors)")
+        return "Closed \(doors)"
+    }
+}
+//MARK: Create class for Command
+class DoorsOperations {
+    let openCommand: DoorCommand
+    let closeCommand: DoorCommand
+
+    init(doors: String) {
+        self.openCommand = OpenCommand(doors:doors)
+        self.closeCommand = CloseCommand(doors:doors)
+    }
+
+    func close() -> String {
+        return closeCommand.execute()
+    }
+
+    func open() -> String {
+        return openCommand.execute()
+    }
 }
