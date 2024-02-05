87cd2eefb58bf593bc09b51a593e72353bd2d3fd
jonysingla <jony.singla19@gmail.com>
2024-02-05 17:39:22 +0530

    [Design Pattern Chain of Mediator] Implementation with example

--- TestApp/TestApp/DesignPattern/BehaviouralDesignPattern/MediatorVC.swift
+++ TestApp/TestApp/DesignPattern/BehaviouralDesignPattern/MediatorVC.swift
@@ -13,17 +13,47 @@ class MediatorVC: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let atc = AirTrafficControl()
+        let boeing747_1 = Boeing747(mediator: atc)
+        let boeing747_2 = Boeing747(mediator: atc)
+
+        boeing747_1.send(message: "Hello, this is boeing747_1 requesting permission to land.", to: boeing747_2)
+    }
+}
+
+protocol Airplane {
+    var name: String { get }
+    func receive(message: String, from: Airplane)
+    func send(message: String, to: Airplane)
+}
+
+class AirTrafficControl {
+    private var airplanes: [Airplane] = []
+    
+    func register(plane: Airplane) {
+        airplanes.append(plane)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func send(message: String, from: Airplane, to: Airplane) {
+        to.receive(message: message, from: from)
     }
-    */
+}
 
+//MARK: 1 Mediator
+class Boeing747: Airplane {
+    var name: String = "Boeing 747"
+    private var mediator: AirTrafficControl
+
+    init(mediator: AirTrafficControl) {
+        self.mediator = mediator
+        self.mediator.register(plane: self)
+    }
+    
+    func receive(message: String, from: Airplane) {
+        print("\(name) received message: '\(message)' from \(from.name)")
+    }
+    
+    func send(message: String, to: Airplane) {
+        mediator.send(message: message, from: self, to: to)
+    }
 }
