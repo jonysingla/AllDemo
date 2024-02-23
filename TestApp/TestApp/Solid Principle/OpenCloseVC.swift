a0405e114867ac2e78909c3096b5ab2e2fc86ca7
jonysingla <jony.singla19@gmail.com>
2024-02-23 16:28:51 +0530

    [Principle Open Close] Comments

--- TestApp/TestApp/Solid Principle/OpenCloseVC.swift
+++ TestApp/TestApp/Solid Principle/OpenCloseVC.swift
@@ -55,3 +55,14 @@ class CircleOpen : AShape {
         return 3.14 * radius * radius
     }
 }
+
+/*
+// MARK:  Swift Open close principle
+ The Open/Closed Principle in Swift states that a class should be open for extension but closed for modification. This means that the behavior of a class can be extended without modifying its source code.
+
+One way to achieve this in Swift is by using protocols and protocol extensions. By defining protocols for different behaviors and providing default implementations in protocol extensions, classes can conform to these protocols and inherit the default behavior without needing to modify the original class.
+
+Another approach is to use inheritance and subclassing. By creating a base class with common functionality and allowing subclasses to override specific methods or properties, new behavior can be added without changing the existing code.
+
+Overall, following the Open/Closed Principle in Swift leads to more maintainable and flexible code, as it allows for easy extension and customization without risking unintended side effects or breaking existing functionality.
+*/
